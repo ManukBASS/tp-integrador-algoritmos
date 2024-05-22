08073fccf9499ca992fe97c710164642b779c164
Manu Zarraga <manuzarraga@hotmail.com>
2024-05-22 20:23:27 -0300

    Agrego trabajo inicial con consigna

---
+++ integrador.py
@@ -0,0 +1,18 @@
+"""
+CONSIGNA: 
+El trabajo práctico permite aplicar conceptos teóricos en un entorno práctico. 
+Se propone que cada equipo encuentre y diseñe una situación problemática.
+Cada grupo será conformado por 4 personas.
+En el desarrollo del TP tendrán que utilizar: 
+* Estructuras básicas (secuencial – decisión – iteración) 
+* Datos estructurados (Listas) – máximos/mínimos, búsqueda, ordenamiento
+* Programación modular - funciones
+* Manejos de datos al azar (random)
+No se pueden utilizar: archivos, manejo de excepciones, diccionarios, tuplas, conjuntos, ciclos 
+infinitos, ruptura de ciclos, programación orientada a objetos, cadena de caracteres (strings)
+
+IDEA: CAJERO AUTOMÁTICO:
+- Ingreso de Dinero
+- Egreso de Dinero
+- Transferencias ??
+"""
